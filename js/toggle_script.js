ddaee95624d57887d5c77b8e5ecb4935192b99fe
Eduardo Azevedo <eduaze@gmail.com>
2018-01-25 00:03:30 -0500

    first commit

---
+++ js/toggle_script.js
@@ -0,0 +1,15 @@
+<script type='text/javascript'>
+//<![CDATA[
+
+        <!--
+  function toggle(obj){
+        var div1 = document.getElementById(obj)
+        if (div1.style.display == 'none') {
+                div1.style.display = 'block'
+        } else {
+                div1.style.display = 'none'
+        }
+  }
+        //-->
+  //]]>
+  </script>
